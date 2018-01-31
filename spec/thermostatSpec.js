7995b3e83ca3d74a16bd0bcffe72d2748b1c867b
Edward <ea.litten@gmail.com>
2018-01-31 17:44:40 +0000

    Add minimum temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -21,5 +21,12 @@ describe('Thermostat',function(){
     thermostat.turnDown();
     expect(thermostat.getTemperature()).toEqual(19);
   });
+
+  it('has a minimum of 10 degrees', function() {
+    for (var i = 0; i < 11; i++) {
+      thermostat.turnDown();
+    }
+    expect(thermostat.getTemperature()).toEqual(10);
+  });
  
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,6 +1,7 @@
 // 'use strict';
 
 function Thermostat() {
+  this.MIN_TEMP = 10;
 this.temperature = 20;
 }
 
@@ -12,10 +13,13 @@ turnUp: function () {
   if (this.isMaximum()) { return }
   this.temperature += 1
 },
-
 turnDown: function () {
   if (this.isMinimum()) { return }
   this.temperature -= 1
+},
+
+isMinimum: function () { 
+  return this.temperature === this.MIN_TEMP 
 },
 
 }
