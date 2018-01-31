93bfb002c5611842b77f2fae95b6b0564a2c13dd
Edward <ea.litten@gmail.com>
2018-01-31 17:41:12 +0000

    Thermostat starts at 20 degrees & can return temp

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('Thermostat',function(){
+
+  var thermostat;
+
+  beforeEach(function(){
+    thermostat = new Thermostat
+  });
+
+  it('starts at 20 degrees',function(){
+    expect(thermostat.getTemperature()).toEqual(20);
+  });
+
+
+ 
+});
---
+++ src/thermostat.js
@@ -0,0 +1,12 @@
+// 'use strict';
+
+function Thermostat() {
+this.temperature = 20;
+}
+
+Thermostat.prototype = {
+  getTemperature: function () { return this.temperature },
+
+  
+
+}
