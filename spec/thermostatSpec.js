43bfeec4aaf660fe2879641e492c71326126be45
Edward <ea.litten@gmail.com>
2018-01-31 17:42:31 +0000

    Add fn to turn up thermostat

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -12,6 +12,9 @@ describe('Thermostat',function(){
     expect(thermostat.getTemperature()).toEqual(20);
   });
 
-
+  it('can increase the temperature with an up function',function(){
+    thermostat.turnUp();
+    expect(thermostat.getTemperature()).toEqual(21);
+  });
  
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -8,5 +8,9 @@ Thermostat.prototype = {
   getTemperature: function () { return this.temperature },
 
   
+turnUp: function () {
+  if (this.isMaximum()) { return }
+  this.temperature += 1
+},
 
 }
