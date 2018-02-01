851b0dcbb71604406eaf9ac07254c6de57f522df
Ed <ea.litten@gmail.com>
2018-02-01 15:38:45 +0000

    Add outside weather for user-entered location
    Co-authored-by: Reena <reena.sanjay.sharma@gmail.com>

--- src/interface.js
+++ src/interface.js
@@ -2,20 +2,37 @@ $(document).ready(function () {
 
   thermostat = new Thermostat()
 
-    function updateTemp(){
-      $("#current-temp").text(thermostat.getTemperature());
-        $("#current-temp").attr('class',thermostat.energyUsage());
-      if(thermostat.isMaximum()) {
-        $("#temperature-up").prop("disabled", true);
-      } else {
-        $("#temperature-up").prop("disabled", false);
-      }
-      if(thermostat.isMinimum()) {
-        $("#temperature-down").prop("disabled", true);
-      } else {
-        $("#temperature-down").prop("disabled", false);
-      }
-    };
+  // get user location
+  $("#user-location").submit(function(event){
+    event.preventDefault();
+    var city = $("#city").val();
+    displayWeather(city);
+  });
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q='
+    var apiKey = '&appid=76acf1fa5703886e3010c6631ef87b4b'
+    var units = '&units=metric'
+    $.get(url + city + units + apiKey, function(weatherResponse){
+      $('#current-weather').text(weatherResponse.main.temp);
+    });
+    $("#current-location").text(city);
+  };
+
+  function updateTemp(){
+    $("#current-temp").text(thermostat.getTemperature());
+      $("#current-temp").attr('class',thermostat.energyUsage());
+    if(thermostat.isMaximum()) {
+      $("#temperature-up").prop("disabled", true);
+    } else {
+      $("#temperature-up").prop("disabled", false);
+    }
+    if(thermostat.isMinimum()) {
+      $("#temperature-down").prop("disabled", true);
+    } else {
+      $("#temperature-down").prop("disabled", false);
+    }
+  };
 
   $("#temperature-up").click(function (event) {
     thermostat.turnUp();
