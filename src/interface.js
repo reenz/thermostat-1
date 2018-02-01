00af8975c2ae5a66f91775059dbae23cb9a3cc2a
reena <reena.sanjay.sharma@gmail.com>
2018-02-01 13:07:54 +0000

    added disabled feature
    Co-authored-by: Ed <ea.litten@gmail.com>

--- src/interface.js
+++ src/interface.js
@@ -2,31 +2,42 @@ $(document).ready(function () {
 
   thermostat = new Thermostat()
 
+    function updateTemp(){
+      $("#current-temp").text(thermostat.getTemperature());
+      if(thermostat.isMaximum()) {
+        $("#temperature-up").prop("disabled", true);
+      } else {
+        $("#temperature-up").prop("disabled", false);
+      }
+      if(thermostat.isMinimum()) {
+        $("#temperature-down").prop("disabled", true);
+      } else {
+        $("#temperature-down").prop("disabled", false);
+      }
+    };
+
   $("#temperature-up").click(function (event) {
     thermostat.turnUp();
-    $("#current-temp").text(thermostat.getTemperature());
-    // if(thermostat.isMaximum()) {
-    //   $("#temperature-up").prop("disabled", true);
-    // }
+    updateTemp();
   });
 
   $("#temperature-down").click(function (event) {
     thermostat.turnDown();
-    $("#current-temp").text(thermostat.getTemperature());
+    updateTemp();
   });
 
   $("#temperature-reset").click(function (event) {
     thermostat.resetTemp();
-    $("#current-temp").text(thermostat.getTemperature());
+    updateTemp();
   });
 
   $("#powersaving-on").click(function (event) {
     thermostat.turnPowerSavingOn();
-    $("#power-saving-status").text(thermostat.isPowerSavingOn());
+    $("#power-saving-status").text ('on');
   });
 
   $("#powersaving-off").click(function (event) {
     thermostat.turnPowerSavingOff();
-    $("#power-saving-status").text(thermostat.isPowerSavingOn());
+    $("#power-saving-status").text('off');
   });
 });
